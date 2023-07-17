8e7df5b62afe9db91b7045a035ecc7df3e2a1b28
PauliusTaraskevicius <fixas1029@gmail.com>
2023-07-17 16:31:46 +0300

    test4

--- hooks/Products/useProducts.ts
+++ hooks/Products/useProducts.ts
@@ -3,7 +3,7 @@ import useSWR from "swr";
 import fetcher from "@/libs/fetcher";
 
 const useProducts = (userId?: string) => {
-  const url = userId ? `/api/products?userId=${userId}` : "api/products";
+  const url = userId ? `/api/products?userId=${userId}` : "/api/products";
   const { data, error, isLoading, mutate } = useSWR(url, fetcher);
 
   return {
